ecd13c74f61060001d8854337ace42493b369e00
VICTOR MAXWELL <chukwumamaxwell2006@gmail.com>
2026-08-28 16:14:09 +0100

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -4,5 +4,5 @@
 
 window.SUPABASE_CONFIG = {
   url: "https://lszfjptybkezuaqtuwue.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxzemZqcHR5bmtlenVhcXR1d3VlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc4MzQ2MTUsImV4cCI6MjEwMzQxMDYxNX0.wVdm05MUPNr-5_KjUjZNdtnVFuDzT5MZdiy2cIDtmCs"
+  anonKey: "sb_publishable_OnyI3Y0PvdsmQWhmRq6mxA_T9c09ghA"
 ;
